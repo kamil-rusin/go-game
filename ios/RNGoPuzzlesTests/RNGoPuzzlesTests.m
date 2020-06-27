f91b68cb87c57b0ca7ca29bdfcf0b1b09a62be94
Kamil Rusin <lotrusek@gmail.com>
2020-06-27 12:50:40 +0200

    feat: rename project

--- ios/RNGoPuzzlesTests/RNGoPuzzlesTests.m
+++ ios/RNGoPuzzlesTests/RNGoPuzzlesTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PrimaryStructureTests : XCTestCase
+@interface RNGoPuzzlesTests : XCTestCase
 
 @end
 
-@implementation PrimaryStructureTests
+@implementation RNGoPuzzlesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
